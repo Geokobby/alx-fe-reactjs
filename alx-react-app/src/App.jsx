0d9dee65acb68a9a1db553a6b76786efe1648670
George Obeng-Boateng <geokobbymail@gmail.com>
2025-07-13 22:39:21 +0000

    App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,17 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
-
+import WelcomeMessage from './WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
+import UserProfile from './UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
       <Header />
       <MainContent />
