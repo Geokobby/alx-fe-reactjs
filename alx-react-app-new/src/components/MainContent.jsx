ca573547000a802d3814af3dcbf7e0d3190ea570
George Obeng-Boateng <geokobbymail@gmail.com>
2025-07-18 11:00:37 +0000

    mainContent

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+    return (
+        <main style={{ border: 'solid 1px gray', padding:'10px', margin:'10px',}}>
+            <p style={{fontWeight:'bolder',}}>I love to visit New York, Paris, and Tokyo.</p>
+      </main>  
+    );
+}
+export default MainContent;
