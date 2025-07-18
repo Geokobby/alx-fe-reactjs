b48f5d2868c4e2dc6b34f89ece3d16b24b888793
George Obeng-Boateng <geokobbymail@gmail.com>
2025-07-18 21:07:30 +0000

    Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 function Navbar() {
     return (
-        <nav style={{ display: 'flex', gap: '1rem', padding: '1rem' }}>
+        <nav style={{ display: 'flex', gap: '1rem', padding: '1rem', backgroundColor: 'blue', justifyContent:'center'}}>
                         <Link to="/">Home</Link>
                         <Link to="/about">About</Link>
                         <Link to="/services">Services</Link>
