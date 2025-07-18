6927f5cec24f560e34e74065fecc9802e06f8c8d
George Obeng-Boateng <geokobbymail@gmail.com>
2025-07-18 20:08:46 +0000

    About.jsx

---
+++ my-company/src/components/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div>
+            <h2>About us</h2>
+            <p>We enjoy building and solving problems</p>
+        </div>
+    );
+}
+
+export default About;
