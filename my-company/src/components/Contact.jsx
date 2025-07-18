2a550a606e4be03e3d3a3f253210acca5a04f6a1
George Obeng-Boateng <geokobbymail@gmail.com>
2025-07-18 20:14:53 +0000

    Added-Contact-Services

---
+++ my-company/src/components/Contact.jsx
@@ -0,0 +1,11 @@
+function Contact() {
+    return (
+        <div>
+            <h2>Get Intouch</h2>
+            <p>we like to her from you </p>    
+        </div>
+    );
+        
+}
+
+export default Contact;
---
+++ my-company/src/components/Services.jsx
@@ -0,0 +1,14 @@
+function Services() {
+    return (
+        <div>
+            <h2> We serve you with..</h2>
+            <ul>
+                <li>Digital Designs</li>
+                <li> Crypto insights</li>
+                <li>Financial tips <span>(Budgetting)</span></li>
+            </ul>
+      </div>  
+    );
+}
+
+export default Services;
