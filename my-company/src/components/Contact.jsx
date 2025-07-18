35a0d27cf582c0ffe2118e7f87191823e0b176e9
George Obeng-Boateng <geokobbymail@gmail.com>
2025-07-18 20:57:18 +0000

    Contact.jsx

--- my-company/src/components/Contact.jsx
+++ my-company/src/components/Contact.jsx
@@ -16,7 +16,7 @@ function Contact() {
      };
 
      return (
-       <div style={{ padding: '20px' }}>
+       <div style={{ padding: '20px', textAlign:'center' }}>
          <h1>Contact Us</h1>
          <form onSubmit={handleSubmit}>
            <input
