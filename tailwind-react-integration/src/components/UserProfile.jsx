dd09d6d3111c576c78e8ee2e5ec3dc876414b1d2
George Obeng-Boateng <geokobbymail@gmail.com>
2025-08-16 12:24:39 +0000

    UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,9 +4,9 @@ function UserProfile() {
           
           <img className="rounded-full w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
           
-          <h1 className="md:text-xl  text-blue-800 my-4 text-lg">John Doe</h1>
+          <h1 className="md:text-xl  text-blue-800 my-4 sm:text-lg">John Doe</h1>
           
-      <p className="text-gray-600 text-base ">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <p className="text-gray-600 md:text-base sm:text-sm ">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
