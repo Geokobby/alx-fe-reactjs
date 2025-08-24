e7f43649a698079c77e194938c756465f7b25b4a
George Obeng-Boateng <geokobbymail@gmail.com>
2025-08-24 18:42:42 +0000

    App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,10 +1,17 @@
 import PostsComponent from "./components/PostsComponent";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   return (
     <div>
+      
       <h1 className="text-3xl font-bold text-center mt-6">React Query Demo</h1>
       <PostsComponent />
+
+      <QueryClientProvider client={queryClient}>
+  <App />
+  <ReactQueryDevtools initialIsOpen={false} />
+</QueryClientProvider>
     </div>
   );
 }
